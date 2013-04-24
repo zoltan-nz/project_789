82686c34235f3fc6312d3b9f18075eaee9c041eb
szines <zdebre@gmail.com>
2013-04-24 02:25:34 +0100

    bootstrap implemented

--- config/initializers/wordless_preferences.php
+++ config/initializers/wordless_preferences.php
@@ -8,13 +8,13 @@ Wordless::set_preference("assets.preprocessors", array("SprocketsPreprocessor", 
 // Wordless::set_preference("assets.cache_enabled", true);
 // Wordless::set_preference("assets.version", get_theme_version());
 
-// Wordless::set_preference("css.compass_path",     "/usr/bin/compass");
+Wordless::set_preference("css.compass_path",     "/usr/local/rvm/gems/ruby-2.0.0-p0/bin/compass");
 // Wordless::set_preference("css.output_style",     "compressed");
 // Wordless::set_preference("css.require_libs",     array());
 
 // Wordless::set_preference("css.lessc_path",       "/usr/bin/lessc");
 // Wordless::set_preference("css.compress",         false);
 
-// Wordless::set_preference("js.ruby_path",         "/usr/bin/ruby");
+Wordless::set_preference("js.ruby_path",         "ruby");
 // Wordless::set_preference("js.yui_compress",      false);
 // Wordless::set_preference("js.yui_munge",         false);
